525a7ff43282755cc0b3bf2a1ae56e3f81328908
PriscilaDenise <denisepriscilamuwanguzi.com>
2024-09-13 09:55:31 +0300

    Finding large element in an array

--- DAA/Assignment/Exercise1.c
+++ DAA/Assignment/Exercise1.c
@@ -8,3 +8,29 @@
 // 5. If it is not, move to the next element in the array. 
 // 6. Return the largest element. 
 
+#include <stdio.h>
+
+int findMaximum(int arr[], int n) {
+    //  Initialize max_element to the first element of the array
+    int max_element = arr[0];
+    
+    // Loop through the array starting from the second element
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_element) {
+            max_element = arr[i]; // Update max_element if a larger value is found
+        }
+    }
+    
+    //  Return the maximum element
+    return max_element;
+}
+
+int main() {
+    int arr[] = {12, 45, 2, 67, 89, 34, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    int max = findMaximum(arr, n);
+    printf("The maximum element in the array is: %d\n", max);
+    
+    return 0;
+}
